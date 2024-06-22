45db0c666658fa3015d28dcc19e8cbb6d871cbec
diamondease <diamondease@gmail.com>
2024-06-22 12:01:39 +0700

    configureid

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './style/Navbar.css';
-
-
 import './style/bootstrap.min.css'; // Mengimpor Bootstrap CSS
+
+
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-CCM5PG82PW");
+// Send pageview with a custom path
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname,
+ });
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
